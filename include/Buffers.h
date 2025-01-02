562d3c2e5d0ddae8bf8f1b915d0b49944c5a9682
Phil Underwood <philjunderwood@gmail.com>
2025-01-02 16:19:38 +0000

    Changed from 3-Clause BSD license to AGPL effective before completion of first release.

--- include/Buffers.h
+++ include/Buffers.h
@@ -1,11 +1,19 @@
-/******************************************************************************
-* JSON In NetCDF Out                                                          *
-*                                                                             *
-* (C) Copyright 2024, Phil Underwood. All rights reserved.                    *
-*                                                                             *
-* This software is licensed under the terms of the 3-Clause BSD License       *
-* which can be obtained from https://opensource.org/license/bsd-3-clause/.    *
-******************************************************************************/
+/***************************************************************************************************
+* JSON In NetCDF Out                                                                               *
+*                                                                                                  *
+* (C) Copyright 2025, Phil Underwood. All rights reserved.                                         *
+*                                                                                                  *
+* This program is free software: you can redistribute it and/or modify it under the terms of the   *
+* GNU General Public License as published by the Free Software Foundation, either version 3 of the *
+* License, or (at your option) any later version.                                                  *
+*                                                                                                  *
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;        *
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See    *
+* the GNU General Public License for more details.                                                 *
+*                                                                                                  *
+* You should have received a copy of the GNU General Public License along with this program. If    *
+* not, see <https://www.gnu.org/licenses/>.                                                        *
+***************************************************************************************************/
 
 #ifndef INCLUDE_BUFFERS_H_
 #define INCLUDE_BUFFERS_H_
